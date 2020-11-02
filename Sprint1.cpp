1b0f578ce8b872c201bc1478bac7caae57216860
Martin Tsifondarov <53626592+MDTsifundarov@users.noreply.github.com>
2020-11-02 11:45:18 +0200

    Create Sprint1.cpp
    first commit

---
+++ Sprint1.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+//въвеждане на елементите на масив
+void input(int arr[], int N)
+{
+	for ([int i = 0; i < N; i++])
+	{
+		cin >> arr[i];
+	}
+}
+int merge(int arr1[], int m, int arr2[], int n, int* mergedArray)
+
+//обединение
+{
+	int temp, count = 0, index = 0; //count - брой повтарящи се елементи
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n - count; j++)
+		{
+			if ([arr1[i] == arr2[j]])
+			{
+				temp = arr2[j];
+				[arr2[j] = arr2[n ‑ 1 ‑ count];]
+				[arr2[n ‑ 1 ‑ count] = temp;]
+				count++;
+			}
+		}
+		mergedArray[index++] = arr1[i];
+	}
+	for (int i = 0; i < n - count; i++)
+	{
+		mergedArray[index++] = arr2[i];
+	}
+	return index;
+}
+// сечение
+int section(int arr1[], int m, int arr2[], int n, int* sectArray)
+{
+	int index = 0;
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if ([arr1[i] == arr2[j]])
+			{
+				sectArray[index++] = arr1[i];
+			}
+		}
+	}
+	return index;
+}
+
+int main()
+{
+	int m, n;
+	cin >> m >> n;
+
+	int* arr1 = new int [m];
+	int* arr2 = new int [n];
+	input(arr1, m);
+	input([arr2, n]);
+
+
+	cout << "Merge: ";
+	int* mergedArray = new int[m + n];
+	int mergeCount = merge(arr1, m, arr2, n, mergedArray);
+	for (int i = 0; i < mergeCount; i++)
+	{
+		cout << mergedArray[i] << " ";
+	}
+
+	
+         cout << endl << "Section: ";
+	int* sectArray = new int[m];
+	int sectCount = section(arr1, m, [arr2, n], sectArray);
+	for (int i = 0; i < sectCount; i++)
+	{
+		cout << sectArray[i] << " ";
+	}
+}
